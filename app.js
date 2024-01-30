343b3ae63e019ad0ee73ac7f1de73ba392ff89d7
olopcodes <olopcodes@gmail.com>
2024-01-29 20:29:40 -0500

    added directional buttons, functionality, and responsive styles

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const btnCopyEl = document.querySelector(".btn-copy");
 const backgroundGradientBoxEl = document.querySelector(".gradient-box");
 const body = document.querySelector("body");
 const gradientTextareaEl = document.querySelector(".gradient-rule");
+const directionalButtonsEl = document.querySelector('.btn-directions');
 
 printColor();
 
@@ -11,18 +12,38 @@ color1InputEl.addEventListener("input", inputHandler);
 
 color2InputEl.addEventListener("input", inputHandler);
 
+directionalButtonsEl.addEventListener('click', directionHandler);
+
+function directionHandler (e) {
+  const btnId = e.target.closest('button').id;
+
+  if(btnId=== 'btn-up') {
+    printColor('up');
+    setBackgroundGradient('top');
+  } else if (btnId=== 'btn-left') {
+    printColor('left');
+    setBackgroundGradient('left');
+  } else if (btnId=== 'btn-right') {
+    printColor('right');
+    setBackgroundGradient('right');
+  } else if (btnId=== 'btn-down') {
+    printColor('down');
+    setBackgroundGradient('bottom');
+  }
+}
+
 btnCopyEl.addEventListener("click", () => {
   gradientTextareaEl.select();
   gradientTextareaEl.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(gradientTextareaEl.value);
 });
 
-function printColor() {
-  gradientTextareaEl.value = `background-image: linear-gradient(to right, ${color1InputEl.value} , ${color2InputEl.value});`;
+function printColor(dir = "right") {
+  gradientTextareaEl.value = `background-image: linear-gradient(to ${dir}, ${color1InputEl.value} , ${color2InputEl.value});`;
 }
 
-function setBackgroundGradient() {
-  body.style.backgroundImage = `linear-gradient(to right, ${color1InputEl.value} , ${color2InputEl.value} )`;
+function setBackgroundGradient(dir = "right") {
+  body.style.backgroundImage = `linear-gradient(to ${dir}, ${color1InputEl.value} , ${color2InputEl.value} )`;
 }
 
 function inputHandler() {
